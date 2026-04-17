4dfa718b7bae87389cc5a45db9760d0a9117b6da
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-04-17 23:47:04 +0500

    Add Stats component and include in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/custom/Hero';
+import Stats from '@/components/custom/Stats';
 
 export const App = () => {
   return (
     <main className='flex flex-col mx-auto p-10 container max-w-4xl lg:max-w-6xl lg:pl-0 lg:pr-10'>
       <Hero />
+      <Stats />
     </main>
   );
 };
