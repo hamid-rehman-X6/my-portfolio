9a0ec0bcf11c87f193eef0a01c3b8333a5f6dc0a
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-04-18 23:06:53 +0500

    use lenis for smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,20 @@ import Services from '@/components/custom/Services';
 import Resume from '@/components/custom/Resume';
 import Testimonials from '@/components/custom/Testimonials';
 import Contact from '@/components/custom/Contact';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 export const App = () => {
+  const lenis = new Lenis();
+
+  useEffect(() => {
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className='flex flex-col mx-auto p-10 container max-w-4xl lg:max-w-6xl lg:pl-0 lg:pr-10'>
       <Hero />
